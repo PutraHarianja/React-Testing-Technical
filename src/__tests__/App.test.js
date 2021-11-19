d43590259d30c301d8baf5da816a25b40a811136
PutraHarianja <putra.harianja007@gmail.com>
2021-11-19 15:55:16 +0700

    initial: Done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,17 +2,78 @@ import { AddTodo } from '../components/helper';
 
 test('Should add todo to the list', () => {
   // Arrange
+  const defaultTodo = [{
+    id: 1,
+    name: 'Learn JSX',
+    isComplete: false,
+  },
+  {
+    id: 2,
+    name: 'Build awasome react app',
+    isComplete: false,
+  },
+  {
+    id: 3,
+    name: 'Ship it',
+    isComplete: false,
+  }
+];
 
-  // Act
+const newTodoList = {
+  id: 3,
+  name: "Learn login",
+  isComplete: false,
+};
+const experied = [newTodoList, ...defaultTodo]
+const expectedTrue = JSON.stringify(experied)
 
-  // Assert
+
+// Act
+const actualList = AddTodo(defaultTodo, newTodoList);
+
+const hasil = JSON.stringify(actualList)
+
+// Assert
+expect(hasil).toBe(expectedTrue);
 })
 
 test('should not mutate the existing todo array', () => {
   // Arrange
+  const todoList = [{
+    id: 1,
+    name: "Learn JSX",
+    isComplete: false,
+  },
+  {
+    id: 2,
+    name: "Build awasome react app",
+    isComplete: false,
+  },
+];
 
-  // Act
+const newValue = {
+  id: 3,
+  name: "Learn login",
+  isComplete: false,
+};
 
-  // Assert
+const expectedtodoList = [{
+    id: 1,
+    name: "Learn JSX",
+    isComplete: false,
+  },
+  {
+    id: 2,
+    name: "Build awasome react app",
+    isComplete: false,
+  },
+];
 
+// Act
+AddTodo(todoList, newValue);
+const actualtodoList = todoList;
+const todoListComparation = JSON.stringify(expectedtodoList) === JSON.stringify(actualtodoList);
+
+// // Assert
+expect(true).toBe(todoListComparation);
 })
